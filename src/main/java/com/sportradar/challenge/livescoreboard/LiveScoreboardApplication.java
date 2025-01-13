f52c4bfc4cd936fbf5094a09ecd9371c6d050437
fahad-git <fahadsiddiqui056@gmail.com>
2025-01-13 23:27:13 +0100

    Merge remote branch with local branch.

---
+++ src/main/java/com/sportradar/challenge/livescoreboard/LiveScoreboardApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.challenge.livescoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveScoreboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveScoreboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sportradar/challenge/livescoreboard/LiveScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportradar.challenge.livescoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveScoreboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
